cd6d753239c16a59c15893707815406123186089
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 14:47:33 +0500

    задача 3 массивы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] myArray = {1,2,3,4,5};
+        int a = myArray[0];
+        myArray[0] = myArray[myArray.length-1];
+        myArray[myArray.length-1] = a;
+        System.out.println(myArray[0]+myArray[myArray.length/2]);
     }
 }
